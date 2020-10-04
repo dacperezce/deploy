e9ca93dac659c1ccc341a9b79bbefef43828503c
dacperezce <dacperezce@unal.edu.co>
2020-10-04 03:42:33 -0500

    Updates

---
+++ precache-manifest.4038f39d8d281257a12527617155d5ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7626dc1efa75e025e4d1c69020e522f9",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "4dda4d63c0b4f6a1aab9",
+    "url": "/deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb9eb714a6268c4910c9",
+    "url": "/deploy/static/js/2.d248ad23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy/static/js/2.d248ad23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dda4d63c0b4f6a1aab9",
+    "url": "/deploy/static/js/main.e86e8050.chunk.js"
+  },
+  {
+    "revision": "45448b4383a81d5b56f2",
+    "url": "/deploy/static/js/runtime-main.0fe5022e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
